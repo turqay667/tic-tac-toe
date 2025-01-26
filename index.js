0ea7e20c9bd7970f7c79d5eeaf992dbc19cf61cd
turgaymammadov <memmedovturqay871@gmail.com>
2025-01-27 00:30:29 +0400

    updated

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cell=document.querySelectorAll(".cell")
 let currentPlayer='X'
 const hint=document.getElementById('hint')
 const result=document.getElementById('result')
+let gameOver=false;
 const winningCells=[
     [0,1,2],
     [3,4,5],
@@ -18,9 +19,12 @@ function checkingScores(player){
     for (let i=0; i<winningCells.length;i++){
         const [a,b,c]=winningCells[i]
      if( cell[a].innerText===currentPlayer && cell[b].innerText===currentPlayer && cell[c].innerText===currentPlayer){
-        setTimeout( ()=>
-          hint.innerText=`Winner is ${currentPlayer}`    
-        )
+        setTimeout(()=>
+          hint.innerText=`Winner is ${player}`,
+            gameOver=true
+
+    )
+        
               return;
        
      }
@@ -30,19 +34,23 @@ function checkingScores(player){
 }
 
 function clickedd(event){
-    if(event.target.innerText===''){
+    if(!gameOver && event.target.innerText===''){
         event.target.innerText=currentPlayer
         checkingScores(currentPlayer)
         currentPlayer=currentPlayer==='X' ? 'O' : 'X'; 
         hint.innerText=`Turn on ${currentPlayer}`
     }
+  
+
 
 }
+
 function reset(){
     for (let i=0;i<=8;i++){
         cell[i].innerText='';      
     }
     currentPlayer='X';
+    gameOver=false;
     hint.innerText='First Player: X'
 
 }
